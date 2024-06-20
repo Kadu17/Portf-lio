f75351e9e5ba367b6c856f9f2e63148aa4cfc933
Carlos <118760023+Kadu17@users.noreply.github.com>
2024-06-20 18:12:36 -0300

    Update Contact.jsx

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,50 +1,76 @@
-import React from 'react'
-import styles from '../sections/ContactStyles.module.css'
+import React, { useState } from 'react';
+import styles from '../sections/ContactStyles.module.css';
+import emailjs from '@emailjs/browser'; 
 
 function Contact() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    const templateParams = {
+      from_name: name,
+      message: message,
+      email: email
+    };
+
+    emailjs.send("service_84ivnen", "template_zahzezt", templateParams, "hQGlN1npLV0Ql2Uav")
+      .then((response) => {
+        console.log("EMAIL ENVIADO", response.status, response.text);
+        setName('');
+        setEmail('');
+        setMessage('');
+      })
+      .catch((error) => {
+        console.log("ERRO: ", error);
+      });
+  };
+
   return (
-    <section id='contact' className={styles.container}>
+    <section className={styles.container}>
       <h1 className="sectionTitle">Contact</h1>
-      <form action=''>
+      <form onSubmit={sendEmail}>
         <div className='formGroup'>
-            <label htmlFor="name" hidden>
-                Name
-            </label>
-            <input
-                type='text'
-                name='name'
-                id='name'
-                placeholder='Name'
-                required
-            />
+          <label htmlFor="name" hidden>Name</label>
+          <input
+            type='text'
+            name='name'
+            id='name'
+            placeholder='Name'
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+            required
+          />
         </div>
         <div className='formGroup'>
-            <label htmlFor="email" hidden>
-                Email
-            </label>
-            <input
-                type='text'
-                name='email'
-                id='email'
-                placeholder='Email'
-                required
-            />
+          <label htmlFor="email" hidden>Email</label>
+          <input
+            type='email' 
+            name='email'
+            id='email'
+            placeholder='Email'
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            required
+          />
         </div>
         <div className='formGroup'>
-            <label htmlFor="message" hidden>
-                Message
-            </label>
-            <textarea
-                type='text'
-                name='message'
-                id='message'
-                placeholder='Message'
-                required></textarea>
+          <label htmlFor="message" hidden>Message</label>
+          <textarea
+            name='message'
+            id='message'
+            placeholder='Message'
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
+            required
+          ></textarea>
         </div>
-        <input className="hover btn" type="submit" value="Submit" />
+        <input className="hover btn" type="submit" value="Submit" autoComplete="name" />
       </form>
     </section>
-  )
+  );
 }
 
-export default Contact
+export default Contact;
